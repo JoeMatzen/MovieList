b2cd844a6262808419913318eeb89a73c0db5038
root <bill@aol.com>
2019-07-25 20:45:44 +0000

    correct port for deploying to heroku

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.use(indexRoutes);
 
 
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Movie-Watchlist app has Started");
 });
